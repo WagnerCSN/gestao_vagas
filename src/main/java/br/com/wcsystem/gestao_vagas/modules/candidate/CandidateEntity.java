2e35b3730a1c26c537694a421b5b206950f4b6d3
WagnerCSN <wagcostvez@gmail.com>
2025-05-28 20:58:37 -0300

    Feat: Add the candidate table to the database

--- src/main/java/br/com/wcsystem/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/wcsystem/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,22 +1,28 @@
 package br.com.wcsystem.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity(name = "candidate")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String username;
     private String email;
     private String password;
     private String description;
     private String curriculum;
-    public CandidateEntity save(CandidateEntity candidateEntity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
-    }
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
