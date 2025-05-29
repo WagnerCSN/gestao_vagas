ab27da5c673cee39ce0763b009240ef7e8634b06
WagnerCSN <wagcostvez@gmail.com>
2025-05-28 21:15:12 -0300

    Refactor: Add validations in the cadidate entity

--- src/main/java/br/com/wcsystem/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/wcsystem/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -17,8 +21,15 @@ public class CandidateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "Username cannot contain spaces!")
     private String username;
+    
+    @Email(message = "Invalid email!")
     private String email;
+
+    @Length(min = 10, max = 100, message = "Password must contain at least 10 characters!")
     private String password;
     private String description;
     private String curriculum;
