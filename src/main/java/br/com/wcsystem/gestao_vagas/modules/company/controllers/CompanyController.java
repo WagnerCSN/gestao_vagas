1b5e0a8fb93939810a3933210958c7594322a693
WagnerCSN <wagcostvez@gmail.com>
2025-06-06 19:09:01 -0300

    Feat: Add CompanyController

---
+++ src/main/java/br/com/wcsystem/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,29 @@
+package br.com.wcsystem.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.wcsystem.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.wcsystem.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+    
+    @Autowired
+    private CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity){
+        try {
+            var result = createCompanyUseCase.execute(companyEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
