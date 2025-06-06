4441ea53249b9be21a5cd09eb3138e93bfa9c741
WagnerCSN <wagcostvez@gmail.com>
2025-06-06 16:55:24 -0300

    Feat: Add companyEntity

---
+++ src/main/java/br/com/wcsystem/gestao_vagas/modules/company/entities/companyEntity.java
@@ -0,0 +1,37 @@
+package br.com.wcsystem.gestao_vagas.modules.company.entities;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class companyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "Username cannot contain spaces!")
+    private String username;
+    
+    @Email(message = "Invalid email!")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "Password must contain at least 10 characters!")
+    private String password;
+    private String website;
+
+    private CreationTimestamp createdAt;
+}
